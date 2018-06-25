e443476cf9656120e66d7b7ceaaed95bccfee0ef
auryn <auryn31@icloud.com>
2018-06-25 10:36:54 +0200

    initial commit

---
+++ src/main/kotlin/aco_flow_shop/Hello.kt
@@ -0,0 +1,6 @@
+package aco_flow_shop
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/aco_flow_shop/HelloTest.kt
@@ -0,0 +1,8 @@
+package aco_flow_shop
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
