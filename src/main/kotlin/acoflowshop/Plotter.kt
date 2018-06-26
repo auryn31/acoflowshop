438e346b716e34d69022f0e9a0e720c55ff79a7b
auryn <auryn31@icloud.com>
2018-06-26 16:18:46 +0200

    update plotter

--- src/main/kotlin/acoflowshop/Plotter.kt
+++ src/main/kotlin/acoflowshop/Plotter.kt
@@ -1,112 +1,65 @@
 package acoflowshop
 
-import com.lowagie.text.Document
-import com.lowagie.text.Rectangle
-import com.lowagie.text.pdf.GrayColor
-import com.lowagie.text.pdf.PdfWriter
-import javafx.scene.chart.CategoryAxis
-import javafx.scene.chart.NumberAxis
-import javafx.scene.chart.StackedBarChart
-import javafx.scene.chart.XYChart
-import javafx.stage.Stage
-import javax.xml.crypto.Data
-import javafx.scene.Scene
-import javafx.scene.layout.VBox
-import org.jCharts.axisChart.AxisChart
-import org.jCharts.chartData.AxisChartDataSet
-import org.jCharts.chartData.DataSeries
-import org.jCharts.encoders.PNGEncoder
-import org.jCharts.properties.*
-import org.jCharts.test.TestDataGenerator
-import org.jCharts.types.ChartType
-import java.io.FileOutputStream
-import java.util.*
-import com.sun.xml.internal.ws.streaming.XMLStreamReaderUtil.close
 import java.awt.*
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
 import javax.imageio.ImageIO
-
+import kotlin.math.log
+import kotlin.math.log10
 
 object Plotter {
     fun plotResults(results: Pair<List<Schedule>, List<Schedule>>, imageName: String) {
 
-//        val xAxisLabel = arrayOf("1", "2")
-//        val xAxisTitle = "Time"
-//        val yAxisTitle = "Machines"
-//        val title = "Scheduling"
-//        val dataSeries = DataSeries(xAxisLabel, xAxisTitle, yAxisTitle, title)
-//
-////        val machineOne = results.first.map { it.job.durationMachineOne }.map { it.toDouble() }.toDoubleArray()
-//
-//        val data1 = mutableListOf<DoubleArray>()
-//        for(i in 0..results.first.size-1) {
-//            val arr = doubleArrayOf(results.first[i].job.durationMachineOne.toDouble(),results.first[i].job.durationMachineOne.toDouble())
-//            data1.add(arr)
-//        }
-//
-//
-//
-////        val data: Array<DoubleArray> = arrayOf(machineOne, machineOne)
-//        val legendLabels = arrayOf("a", "b", "3")
-//        val paints = TestDataGenerator.getRandomPaints(3)
-//        val paints1 = arrayOf(GradientPaint(1f, 2f, Color(1,2,3), 1f, 2f, Color(1,2,3)),
-//                GradientPaint(1f, 2f, Color(1,2,3), 1f, 2f, Color(1,2,3)),
-//                GradientPaint(1f, 2f, Color(55,180,180), 1f, 2f, Color(55,180,180)))
-//        val stackedBarChartProperties = StackedBarChartProperties()
-//        val axisChartDataSet = AxisChartDataSet(data1.toTypedArray(), legendLabels, paints1, ChartType.BAR_STACKED, stackedBarChartProperties)
-//        dataSeries.addIAxisPlotDataSet(axisChartDataSet)
-//
-//        val chartProperties = ChartProperties()
-//        val axisProperties = AxisProperties(true)
-//        val legendProperties = LegendProperties()
-//        val axisChart = AxisChart(dataSeries, chartProperties, axisProperties, legendProperties, 800, 350)
-//
-//        val output = FileOutputStream("$imageName.png")
-//
-//        PNGEncoder.encode(axisChart, output)
+        val shiftRight = 35
+        val height = 40
 
+        val jobWidth = results.second.last().job.durationMachineTwo + results.second.last().start
+
+        val factor = when (jobWidth) {
+            in 0..10 -> 100
+            in 10..20 -> 50
+            in 20..30 -> 25
+            else -> 12
+        }
 
 
         try {
-            val width = 1000
-            val height = 200;
+            val imageWidth = jobWidth*factor+shiftRight*2
+            val imageHeight = 150;
 
             // TYPE_INT_ARGB specifies the image format: 8-bit RGBA packed
             // into integer pixels
-            val bi = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            val bi = BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
 
             val ig2 = bi.createGraphics();
 
-            val colors = arrayOf(Color.BLUE, Color.CYAN, Color.GRAY, Color.MAGENTA, Color.ORANGE, Color.YELLOW, Color.GREEN)
+            val colors = arrayOf(Color.BLUE, Color.ORANGE, Color.CYAN, Color.GREEN, Color.GRAY, Color.MAGENTA, Color.YELLOW)
+
+            val font = Font("TimesRoman", Font.BOLD, 20);
+            ig2.setFont(font);
+
+            ig2.setPaint(Color.black);
+            ig2.drawString("M1", 5 , height);
+            ig2.drawString("M2", 5, height+50);
 
             for (i in 0 until results.first.size) {
-                ig2.color = colors[Random().nextInt(colors.size)]
-                ig2.drawRect(results.first[i].start*100, 10, results.first[i].job.durationMachineOne*100, 40)
-                ig2.fillRect(results.first[i].start*100, 10, results.first[i].job.durationMachineOne*100, 40)
-                ig2.drawRect(results.second[i].start*100, 60, results.first[i].job.durationMachineTwo*100, 40)
-                ig2.fillRect(results.second[i].start*100, 60, results.first[i].job.durationMachineTwo*100, 40)
+                ig2.color = colors[i%colors.size]
+                ig2.drawRect(shiftRight + results.first[i].start*factor, 10, results.first[i].job.durationMachineOne*factor, height)
+                ig2.fillRect(shiftRight + results.first[i].start*factor, 10, results.first[i].job.durationMachineOne*factor, height)
+                ig2.drawRect(shiftRight + results.second[i].start*factor, 60, results.second[i].job.durationMachineTwo*factor, height)
+                ig2.fillRect(shiftRight + results.second[i].start*factor, 60, results.second[i].job.durationMachineTwo*factor, height)
+                val message = "${i+1}";
+                ig2.setPaint(Color.black)
+                ig2.drawString(message, shiftRight+results.first[i].start*factor + results.first[i].job.durationMachineOne*factor/2 , height)
+                ig2.drawString(message, shiftRight+results.second[i].start*factor + results.second[i].job.durationMachineTwo*factor/2 , height+50)
             }
 
+            ig2.drawLine(shiftRight, imageHeight-shiftRight, jobWidth*factor + shiftRight, imageHeight-shiftRight)
 
-//            val font = Font("TimesRoman", Font.BOLD, 20);
-//            ig2.setFont(font);
-//            val message = "www.java2s.com!";
-//            val fontMetrics = ig2.getFontMetrics();
-//            val stringWidth = fontMetrics.stringWidth(message);
-//            val stringHeight = fontMetrics.getAscent();
-//            ig2.setPaint(Color.black);
-//            ig2.drawString(message, (width - stringWidth) / 2, height / 2 + stringHeight / 4);
-//            ig2.drawRect(10, 10, 20, 30)
-//            ig2.color = Color.BLUE
-//            ig2.fillRect(10, 10, 20, 30)
-//            val shape = java.awt.Rectangle()
-//            shape.height = 10
-//            shape.x = 20
-//            shape.y = 20
-//            shape.width = 100
-//            ig2.draw(shape)
+            val message = "$jobWidth";
+            ig2.setPaint(Color.black)
+            ig2.drawString(message, jobWidth*factor + shiftRight, imageHeight-shiftRight+8)
 
             ImageIO.write(bi, "PNG", File("$imageName.png"));
 
--- src/test/kotlin/acoflowshop/PlotterTest.kt
+++ src/test/kotlin/acoflowshop/PlotterTest.kt
@@ -19,4 +19,25 @@ class PlotterTest {
         val file = File("$fileName.png")
         assertTrue(file.delete())
     }
+
+    @Test
+    fun plotSecondJob(){
+        val fileName = "realDataTestPlot"
+        val pairs = getShortestSchedulePair(listOf<Job>(
+                Job(1,2,1),
+                Job(7,1,1),
+                Job(7,1,1),
+                Job(3,1,1),
+                Job(3,1,1),
+                Job(62,5,1),
+                Job(6,5,1),
+                Job(6,5,1),
+                Job(6,5,1),
+                Job(3,1,1)
+        ), 3)
+
+        Plotter.plotResults(pairs, fileName)
+        val file = File("$fileName.png")
+        assertTrue(file.delete())
+    }
 }
