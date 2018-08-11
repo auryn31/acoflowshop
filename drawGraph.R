ee35c080cecf30cd06b1a0e0859aa4bcdcaf0fff
auryn <auryn31@icloud.com>
2018-08-11 15:04:20 +0200

    update plotter

--- drawGraph.R
+++ drawGraph.R
@@ -5,19 +5,19 @@ bestSolution <- c(ResultMatrix[2]$V2)
 duration <- c(ResultMatrix[3]$V3)
 
 jpeg('current.jpg')
-# p_range <- range(bestSolution, duration, iteration)
+# p_range <- range(bestSolution, duration)
 
-iteration <- lapply(iteration, as.numeric)
-plot(iteration, bestSolution, type="l", xaxt = "n", col="blue", ann=FALSE) # , ylim=p_range
+#duration <- lapply(duration, as.numeric)
+plot(iteration, bestSolution, type="l", xaxt = "n", col="blue", ann=FALSE) # 
 
-axis(1, iteration)
+axis(1, at=1:length(duration), lab=duration)
 
 box()
 
 # lines(duration, type="l", col="green")
 
 title(main="ACO Flow Shop", col.main="red", font.main=4)
-title(xlab="Iteration")
+title(xlab="Dauer in s")
 title(ylab="Länge")
 
 #legend(1, p_range[2], c("Lösungslänge", "Dauer"), cex=0.8, col=c("blue", "green", "red", "cyan", "gold"), pch=21:21, lty=1:1)
