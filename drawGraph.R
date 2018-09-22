1cbbfe4585253aabb370d7a16ef155390db6d093
auryn <auryn31@icloud.com>
2018-09-22 22:21:17 +0200

    update rscript to use function

--- drawGraph.R
+++ drawGraph.R
@@ -8,39 +8,34 @@ bestSolutionACO <- c(ResultMatrix[2]$V2[1:600])
 durationACO <- c(ResultMatrix[3]$V3[1:600])/1000
 evaluationNumberACO <- c(ResultMatrix[4]$V4[1:600])
 reworkPercentACO <- c(ResultMatrix[5]$V5[1:600])
+simulationACO <- c(ResultMatrix[4]$V4[1:600])
 
 bestSolutionAICA <- c(ResultMatrix[2]$V2[601:length(iteration)])
 durationAICA <- c(ResultMatrix[3]$V3[601:length(iteration)])/1000
 evaluationNumberAICA <- c(ResultMatrix[4]$V4[601:length(iteration)])
 reworkPercentAICA <- c(ResultMatrix[5]$V5[601:length(iteration)])
+simulationAICA <- c(ResultMatrix[4]$V4[601:length(iteration)])
 
 # bestSolutionNEH <- c(ResultMatrix[2]$V2)[length(iteration):length(iteration)]
 # durationNEH <- c(ResultMatrix[3]$V3)[length(iteration):length(iteration)]/1000
-jpeg('result_time.jpg')
-range <- range(bestSolutionAICA, bestSolutionACO)
-xrange <- range(durationAICA, durationACO)
 
-#duration <- lapply(duration, as.numeric)
-plot(durationACO, bestSolutionACO, type="l", ylim=range, xlim=xrange, col="blue", ann=FALSE)#, xaxt = "n"
-lines(durationAICA, bestSolutionAICA, type="l", col="green")
-box()
+drawPlot <- function(name, title, xtitle, ytitle, x1, x2, y1, y2) {
+    jpeg(name)
+    yrange <- range(y1, y2)
+    xrange <- range(x1, x2)
+    xrange
 
-title(main="ACO Flow Shop", col.main="red", font.main=4)
-title(xlab="Dauer in s")
-title(ylab="Länge")
+    plot(x1, y1, type="l", ylim=yrange, xlim=xrange, col="blue", ann=FALSE)
+    lines(x2, y2, type="l", col="green")
+    box()
 
-jpeg('result_rework.jpg')
-range <- range(reworkPercentACO, reworkPercentAICA)
-plot(durationACO, reworkPercentACO, type="l", ylim=range, xlim=xrange, col="blue", ann=FALSE)#, xaxt = "n"
-lines(durationAICA, reworkPercentAICA, type="l", col="green")
+    title(main=title, col.main="red", font.main=4)
+    title(xlab=xtitle)
+    title(ylab=ytitle)
+}
 
-# lines(durationNEH, bestSolutionNEH, type="p", col="green")
-# axis(1, at=1:length(durationACO), lab=durationACO)
-
-box()
-
-title(main="ACO Flow Shop Rework", col.main="red", font.main=4)
-title(xlab="Dauer in s")
-title(ylab="Rework in %")
+drawPlot('result_time.jpg', 'ACO Flow Shop', 'Dauer in s', 'Länge', durationACO, durationAICA, bestSolutionACO, bestSolutionAICA)
+drawPlot('result_rework.jpg', 'ACO Flow Shop Rework', 'Dauer in s', 'Rework in %', durationACO, durationAICA, reworkPercentACO, reworkPercentAICA)
+drawPlot('result_simulation.jpg', 'ACO Flow Shop Simulation', 'Simulation', 'Länge', simulationACO, simulationAICA, bestSolutionACO, bestSolutionAICA)
 
 #legend(1, p_range[2], c("Lösungslänge", "Dauer"), cex=0.8, col=c("blue", "green", "red", "cyan", "gold"), pch=21:21, lty=1:1)
