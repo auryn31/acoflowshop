ecfb40a4078ee1647ef92ccfa9000db0fdbc8fbf
auryn <auryn31@icloud.com>
2018-11-29 10:57:34 +0100

    update csv

--- overview_finish.R
+++ overview_finish.R
@@ -9,5 +9,5 @@ lines(overview$jobs, overview$aica, type="p", col="green")
 lines(overview$jobs, overview$neh, type="p", col="red")
 box()
 
-title(main="", xlab="Jobanzahl", ylab="Laufzeit", font.main=4, cex.lab=1.4, cex.axis=1.4, cex.main=1.4, cex.sub=1.4)
+title(main="", xlab="Jobanzahl", ylab="Laufzeit in s", font.main=4, cex.lab=1.4, cex.axis=1.4, cex.main=1.4, cex.sub=1.4)
 legend("topleft", legend=c("ACO", "AICA", "NEH"), col=c("blue", "green", "red"), lty=1:1, cex=1.5)
